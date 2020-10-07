71bd23613af88d7b394e2f209cc2bb2146d0736f
Luis Salinas <luis.salinasp@utem.cl>
2020-10-07 18:09:35 -0300

    Se modifican parametros para generar WAR para despliegue en Tomcat

--- API-Rest/src/main/java/com/api/rest/ApiRestApplication.java
+++ API-Rest/src/main/java/com/api/rest/ApiRestApplication.java
@@ -2,9 +2,16 @@ package com.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiRestApplication {
+public class ApiRestApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApiRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
